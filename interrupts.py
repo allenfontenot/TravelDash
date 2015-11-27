edf2cd5b7d9e202d64de7b981fafdc78732bc9e5
allenfontenot <allen.fontenot@gmail.com>
2015-11-27 08:38:24 -0600

    Create interrupts.py

---
+++ interrupts.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import csv
+import datetime
+from settings import *
+from datetime import timedelta
+from datetime import datetime
+
+
+def interrupt1(channel):
+        print"falling edge 2"
+
+        outputFile = open('travellog.csv', 'a')
+        time1 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time1, zone4, NSN])
+        outputFile.close()
+def interrupt2(channel):
+        print"falling edge 3"
+        outputFile = open('travellog.csv', 'a')
+        time2 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time2, zone5, NSN])
+        outputFile.close()
+def interrupt3(channel):
+        print"falling edge 4"
+        outputFile = open('travellog.csv', 'a')
+        time3 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time3, zone6, NSN])
+        outputFile.close()
+def interrupt4(channel):
+        print"falling edge 14"
+        outputFile = open('travellog.csv', 'a')
+        time1 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time1, zone1, NSN])
+        outputFile.close()
+def interrupt5(channel):
+        print"falling edge 15"
+        outputFile = open('travellog.csv', 'a')
+        time2 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time2, zone2, NSN])
+        outputFile.close()
+def interrupt6(channel):
+        print"falling edge 18"
+        outputFile = open('travellog.csv', 'a')
+        time3 = datetime.now()
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow([time3, zone3, NSN])
+        outputFile.close()
