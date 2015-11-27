366305156bdc8df29c35dc836f8773e199c5ec4a
allenfontenot <allen.fontenot@gmail.com>
2015-11-27 09:25:11 -0600

    Update cronsetup.sh

--- cronsetup.sh
+++ cronsetup.sh
@@ -1,3 +1,3 @@
-sudo cp ~/TravelDash/Cron/online /etc/cron.d/online
-sudo cp ~/TravelDash/Cron/offline /etc/cron.d/offline
-sudo cp ~/TravelDash/Cron/dashonrestart /etc/cron.d/dashonrestart
+sudo cp /home/pi/TravelDash/Cron/online /etc/cron.d/online
+sudo cp /home/pi/TravelDash/Cron/offline /etc/cron.d/offline
+sudo cp /home/pi/TravelDash/Cron/dashonrestart /etc/cron.d/dashonrestart
