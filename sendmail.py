66fd6be150e14de6a7fc07c16e4d6369bb0b3459
allenfontenot <allen.fontenot@gmail.com>
2015-11-27 09:01:07 -0600

    Create sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import smtplib
+import csv
+
+
+def sendmail(z1time, z2time, z3time,store,level):
+
+	l1m = []
+	l2m = []
+	l3m = []
+	
+	fromaddr = 'travelpathnotcomplete@gmail.com'
+	msg = 'Last Travel Paths at ' + str(store) + '\nZone 1: ' + str(z1time) + ' minutes ago \nZone 2: ' + str(z2time) + ' minutes ago \nZone 3: ' + str(z3time) + ' minutes ago' 
+	msg2 = str(store) + " " + str(z1time)
+	username = 'travelpathnotcomplete@gmail.com'
+	password = 'gehrig10'
+	server = smtplib.SMTP('smtp.gmail.com:587')
+
+	#fills array with emails in files. must be one per line
+	with open('emaillevel1.txt', 'r') as f:
+		l1m = [line.strip() for line in f]
+
+	with open('emaillevel2.txt', 'r') as f:
+                l2m = [line.strip() for line in f]
+
+	with open('emaillevel3.txt', 'r') as f:
+                l3m = [line.strip() for line in f]
+
+	server.starttls()
+        server.login(username,password)
+
+	#send one email for each address in array
+	if   level == 1:
+		for elem in l1m:
+			toaddr = elem
+			server.sendmail(fromaddr, toaddr, msg)		
+	elif level == 2:
+		for elem in l1m:
+                        toaddr = elem
+                        server.sendmail(fromaddr, toaddr, msg)
+
+	elif level == 3:
+		for elem in l1m:
+                        toaddr = elem
+                        server.sendmail(fromaddr, toaddr, msg)
+	elif level == 5:
+		toaddr = "allen.fontenot@gmail.com"
+		server.sendmail(fromaddr, toaddr, msg2)
+        server.quit()
