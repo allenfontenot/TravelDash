a874cee0209f02e3559323d50a666c9e4acddc5f
allenfontenot <allen.fontenot@gmail.com>
2015-11-28 09:34:27 -0600

    Update cronsetup.sh

--- cronsetup.sh
+++ cronsetup.sh
@@ -7,4 +7,9 @@ sudo chmod +x /home/pi/TravelDash/initlog.py
 sudo cp /home/pi/TravelDash/Crons/online /etc/cron.d/online
 sudo cp /home/pi/TravelDash/Crons/offline /etc/cron.d/offline
 sudo cp /home/pi/TravelDash/Crons/dashonrestart /etc/cron.d/dashonrestart
+
+sudo chmod +x /etc/cron.d/online
+sudo chmod +x /etc/cron.d/offline
+sudo chmod +x /etc/cron.d/dashonrestart
+
 sudo python initlog.py
