94f0e80fd50fc39045a370ead11e60980f2fc000
Allen <allen.fontenot@gmail.com>
2015-12-01 06:47:54 -0600

    removed import os...was unused removed other unused import moved the send mail logic to its own function in background and fixed a bug not sending mail to level2 and 3 lists fixed uninitialized variable in background from move

--- TravelDash.py
+++ TravelDash.py
@@ -174,7 +174,7 @@ while True:
             			lm = tm
 #level two email
         if thenMinutes[0] >= mailtimeLevel2 or thenMinutes[1] >= mailtimeLevel2 or thenMinutes[2] >= mailtimeLevel2:
-		        lvl = 2
+                lvl = 2
                 tm2 = time.time()  #this mail is now
                 tslm2 = tm2 - lm2    #time since last mail = this mail - last mail
                 if tslm2 > tba:#time between alerts in seconds 30min = 1800
