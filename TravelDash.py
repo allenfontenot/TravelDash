b1e4a78850093b97ca7cc75159bcbf3e1691acdb
Allen <allen.fontenot@gmail.com>
2015-12-01 06:35:27 -0600

    removed import os...was unused removed other unused import moved the send mail logic to its own function in background and fixed a bug not sending mail to level2 and 3 lists fixed uninitialized variable in background from move

--- TravelDash.py
+++ TravelDash.py
@@ -41,6 +41,7 @@ imyellow = [False,False,False]
 imred = [False,False,False]
 
 #mail timers for not spamming notifications
+tm = tm2 =  0; lm = lm2 =  0;
 thenRows = 0
 logcount = 0
 logcount2 = 0
@@ -161,6 +162,25 @@ while True:
 					lcd.blit(background, (0,0))
 					pygame.display.flip()
 			k += 1
-		notify()
+		#notify(lm)
+		if thenMinutes[0] >= mailtimeLevel1 or thenMinutes[1] >= mailtimeLevel1 or thenMinutes[2] >= mailtimeLevel1:
+        lvl = 1
+        tm = time.time()  #this mail is now
+        tslm = tm - lm    #time since last mail = this mail - last mail
+        if tslm > tba:#time between alerts in seconds 30min = 1800
+            sendmail(thenMinutes[0], thenMinutes[1], thenMinutes[2],NSN,lvl)
+            print "level 1 email sent at " + str(datetime.now())
+            logging.debug(str(datetime.now()) + " level 1 email sent")
+            lm = tm
+#level two email
+	    if thenMinutes[0] >= mailtimeLevel2 or thenMinutes[1] >= mailtimeLevel2 or thenMinutes[2] >= mailtimeLevel2:
+                lvl = 2
+                tm2 = time.time()  #this mail is now
+                tslm2 = tm2 - lm2    #time since last mail = this mail - last mail
+                if tslm2 > tba:#time between alerts in seconds 30min = 1800
+                    sendmail(thenMinutes[0], thenMinutes[1], thenMinutes[2],NSN,lvl)
+                    print "level 2 email sent at " +  str(datetime.now())
+                    logging.debug(str(datetime.now()) +  " level 2 email sent")
+                    lm2 = tm2
 	logcount2 = 0
 	closed = os.path.exists('imhere.txt')
