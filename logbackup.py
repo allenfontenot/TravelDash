bb39044cb6d77803ec6f33c8f5211873a1819f31
Allen <allen.fontenot@gmail.com>
2015-12-02 09:33:20 -0600

    fixed offline check not in main loop

--- logbackup.py
+++ logbackup.py
@@ -1,10 +1,15 @@
 import os
 import shutil
 from settings import NSN
+import datetime
 
-date = '20150831'#get day month year
 
-newFileName = 'TL' + NSN + date + '.csv'
+date = datetime.date.today()#get day month year
+
+#moves current travellog.csv to log folder and renames travellog.old which is initialized
+#to travellog.csv. So backs up and clears the log
+
+newFileName = 'TL' + NSN + str(date) + '.csv'
 newFilePath = '/home/pi/TravelDash/logs/'+newFileName
 
 
