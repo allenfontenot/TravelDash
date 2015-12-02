5193f6dd084f4754ecbaa48fe59931b9b467ddb4
Allen <allen.fontenot@gmail.com>
2015-12-02 09:23:53 -0600

    fixed offline check not in main loop

--- logbackup.py
+++ logbackup.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from settings import NSN
 
 date = '20150831'#get day month year
@@ -7,4 +8,6 @@ newFileName = 'TL' + NSN + date + '.csv'
 newFilePath = '/home/pi/TravelDash/logs/'+newFileName
 
 
-os.rename('travellog.csv',newFilePath)
+os.rename('/home/pi/TravelDash/travellog.csv',newFilePath)
+
+shutil.copyfile('/home/pi/TravelDash/travellog.old','/home/pi/TravelDash/travellog.csv')
