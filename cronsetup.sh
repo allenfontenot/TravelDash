d6eb0bee8b026b2c7a1c9bcca6bad4a6cb85a0fd
Allen <allen.fontenot@gmail.com>
2015-12-07 08:28:28 -0600

    added logbackup to cronsetup and

--- cronsetup.sh
+++ cronsetup.sh
@@ -3,13 +3,18 @@ sudo chmod +x /home/pi/TravelDash/offline.sh
 sudo chmod +x /home/pi/TravelDash/online.sh
 sudo chmod +x /home/pi/TravelDash/rebootme.sh
 sudo chmod +x /home/pi/TravelDash/initlog.py
+sudo chmod +x /home/pi/TravelDash/logbackup.py
+sudo chmod +x /home/pi/TravelDash/logbackup.sh
+
 
 sudo cp /home/pi/TravelDash/Crons/online /etc/cron.d/online
 sudo cp /home/pi/TravelDash/Crons/offline /etc/cron.d/offline
 sudo cp /home/pi/TravelDash/Crons/dashonrestart /etc/cron.d/dashonrestart
+sudo cp /home/pi/TravelDash/Crons/dashonrestart /etc/cron.d/logbackup
 
 sudo chmod +x /etc/cron.d/online
 sudo chmod +x /etc/cron.d/offline
 sudo chmod +x /etc/cron.d/dashonrestart
+sudo chmod +x /etc/cron.d/logbackup
 
 sudo python initlog.py
