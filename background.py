f85454c3f917ee308c43d117846ab55cd1ed51ed
af <allen.fontenot@gmail.com>
2015-12-08 10:31:42 -0600

    changed initlog to simulate 3 button pushes

--- background.py
+++ background.py
@@ -235,7 +235,9 @@ def avgcomp():  # positions are in complications
         for row in reader:
             if row[2] != 0:
                 zd1.append(int(row[2]))
+    print zd1
     a = sum(zd1) / len(zd1)
+    print a
     complications(2)
     compnumber(2, a)
     comptext(2, c2t)
