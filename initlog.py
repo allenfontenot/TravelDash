27a041519ec79a74e59be6c26664f32e7f9f453c
af <allen.fontenot@gmail.com>
2015-12-08 10:04:00 -0600

    changed initlog to simulate 3 button pushes

--- initlog.py
+++ initlog.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import csv
 import datetime
-from settings import *
 
+zone1 = 'Lobby'
+zone2 = 'Stockroom'
+zone3 = 'Drive Thru'
+time1=[]
+time2=[]
+time3=[]
 
 def findLastTime(zone):
     with open('travellog.csv') as csvfile:
@@ -14,25 +19,36 @@ def findLastTime(zone):
                 time2.append(row[0])
             if row[1].strip() == zone3:
                 time3.append(row[0])
-
+        if zone == 1:
+            return max(time1)
+        elif zone == 2:
+            return max(time2)
+        elif zone == 3:
+            return max(time3)
+        elif zone == 4:
+            return max(time4)
+        elif zone == 5:
+            return max(time5)
+        elif zone == 6:
+            return max(time6)
 
 print"simpush"
 outputFile = open('travellog.csv', 'a')
 now = datetime.datetime.now()
 
-lt1 = datetime.datetime.strptime(str(findLastTime(1)), FMT)
+lt1 = datetime.datetime.strptime(findLastTime(1), FMT)
 td1 = now - lt1
 e1 = int(td1.total_seconds() / 60)
 outputWriter = csv.writer(outputFile)
 outputWriter.writerow([now, zone1, e1, NSN])
 
-lt2 = datetime.datetime.strptime(str(findLastTime(2)), FMT)
+lt2 = datetime.datetime.strptime(findLastTime(2), FMT)
 td2 = now - lt2
 e2 = int(td2.total_seconds() / 60)
 outputWriter = csv.writer(outputFile)
 outputWriter.writerow([now, zone2, 2, NSN])
 
-lt3 = datetime.datetime.strptime(str(findLastTime(3)), FMT)
+lt3 = datetime.datetime.strptime(findLastTime(3), FMT)
 td3 = now - lt3
 e3 = int(td3.total_seconds() / 60)
 outputWriter = csv.writer(outputFile)
